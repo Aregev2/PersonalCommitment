b586967681472a0a834d28f9e68dfa293048462e
Aregev2 <33214461+Aregev2@users.noreply.github.com>
2018-01-11 17:06:44 +0200

    another commit

--- app/src/main/java/com/aregev2/personalcommitment/MainActivity.java
+++ app/src/main/java/com/aregev2/personalcommitment/MainActivity.java
@@ -1,107 +1,286 @@
 package com.aregev2.personalcommitment;
 
+import android.net.Uri;
 import android.os.Bundle;
+import android.os.Handler;
 import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.NavigationView;
 import android.support.design.widget.Snackbar;
+import android.support.v4.app.Fragment;
+import android.support.v4.app.FragmentTransaction;
 import android.support.v4.view.GravityCompat;
 import android.support.v4.widget.DrawerLayout;
 import android.support.v7.app.ActionBarDrawerToggle;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
-import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
+import android.widget.ImageView;
 import android.widget.TextView;
 
 import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.auth.FirebaseUser;
 
-public class MainActivity extends AppCompatActivity implements NavigationView.OnNavigationItemSelectedListener {
 
-    FirebaseAuth auth = FirebaseAuth.getInstance();
-    FirebaseUser usr = FirebaseAuth.getInstance().getCurrentUser();
+public class MainActivity extends AppCompatActivity implements VolunteerFragment.OnFragmentInteractionListener{
+
+    private NavigationView navigationView;
+
+    private DrawerLayout drawerLayout;
+
+    private View navHeader;
+
+    private ImageView imageViewBackground, imageViewPhoto;
+
+    private TextView textViewUsername, textViewEmail;
+
+    private Toolbar toolbar;
+
+    private FloatingActionButton floatingActionButton;
+
+    public static int navItemIndex = 0;
+
+    private static final String VOLUNTEER = "Volunteer";
+    private static final String REPORTS = "Reports";
+    private static final String PROFILE = "Profile";
+    public static String CURRENT_TAG = VOLUNTEER;
+    private String[] activityTitles;
+    private boolean shouldLoadHomeFragOnBackPress = true;
+
+    private Handler mHandler;
+
+    private FirebaseUser firebaseUser;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
+        toolbar = findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
 
-        FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
-        fab.setOnClickListener(new View.OnClickListener() {
+        mHandler = new Handler();
+
+        drawerLayout = findViewById(R.id.drawer_layout);
+        navigationView = findViewById(R.id.nav_view);
+        floatingActionButton = findViewById(R.id.fab);
+
+        navHeader = navigationView.getHeaderView(0);
+        textViewUsername = findViewById(R.id.name);
+        textViewEmail = findViewById(R.id.website);
+        imageViewBackground = findViewById(R.id.img_header_bg);
+        imageViewPhoto = findViewById(R.id.img_profile);
+
+        activityTitles = getResources().getStringArray(R.array.array_fragment_titles);
+
+        firebaseUser = FirebaseAuth.getInstance().getCurrentUser();
+
+        floatingActionButton.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
                 Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG).setAction("Action", null).show();
             }
         });
 
+        loadNavHeader();
+        setUpNavigationView();
+
+        if (savedInstanceState == null) {
+            navItemIndex = 0;
+            CURRENT_TAG = VOLUNTEER;
+            loadHomeFragment();
+        }
+    }
+
+    /***
+     * Load navigation menu header information
+     * like background image, profile image
+     * name, website, notifications action view (dot)
+     */
+    private void loadNavHeader() {
+        String username = firebaseUser.getDisplayName();
+        String email = firebaseUser.getEmail();
+
+        textViewUsername.setText(username);
+        textViewEmail.setText(email);
+    }
+
+    /***
+     * Returns respected fragment that user
+     * selected from navigation menu
+     */
+    private void loadHomeFragment() {
+        // selecting appropriate nav menu item
+        selectNavMenu();
+
+        // set toolbar title
+        setToolbarTitle();
+
+        // if user select the current navigation menu again, don't do anything
+        // just close the navigation drawer
+        if (getSupportFragmentManager().findFragmentByTag(CURRENT_TAG) != null) {
+            drawerLayout.closeDrawers();
+
+            // show or hide the fab button
+            toggleFab();
+            return;
+        }
+
+        // Sometimes, when fragment has huge data, screen seems hanging
+        // when switching between navigation menus
+        // So using runnable, the fragment is loaded with cross fade effect
+        // This effect can be seen in GMail app
+        Runnable mPendingRunnable = new Runnable() {
+            @Override
+            public void run() {
+                // update the main content by replacing fragments
+                Fragment fragment = getHomeFragment();
+                FragmentTransaction fragmentTransaction = getSupportFragmentManager().beginTransaction();
+                fragmentTransaction.setCustomAnimations(android.R.anim.fade_in, android.R.anim.fade_out);
+                fragmentTransaction.replace(R.id.frame, fragment, CURRENT_TAG);
+                fragmentTransaction.commitAllowingStateLoss();
+            }
+        };
+
+        // If mPendingRunnable is not null, then add to the message queue
+        if (mPendingRunnable != null) {
+            mHandler.post(mPendingRunnable);
+        }
+
+        // show or hide the fab button
+        toggleFab();
+
+        //Closing drawer on item click
+        drawerLayout.closeDrawers();
+
+        // refresh toolbar menu
+        invalidateOptionsMenu();
+    }
+
+    private Fragment getHomeFragment() {
+        switch (navItemIndex) {
+            case 0:
+                VolunteerFragment volunteerFragment = new VolunteerFragment();
+                return volunteerFragment;
+            case 1:
+                ReportsFragment reportsFragment = new ReportsFragment();
+                return reportsFragment;
+            case 2:
+                ProfileFragment profileFragment = new ProfileFragment();
+                return profileFragment;
+            default:
+                return new VolunteerFragment();
+        }
+    }
+
+    private void setToolbarTitle() {
+        getSupportActionBar().setTitle(activityTitles[navItemIndex]);
+    }
+
+    private void selectNavMenu() {
+        navigationView.getMenu().getItem(navItemIndex).setChecked(true);
+    }
+
+    private void setUpNavigationView() {
+        //Setting Navigation View Item Selected Listener to handle the item click of the navigation menu
+        navigationView.setNavigationItemSelectedListener(new NavigationView.OnNavigationItemSelectedListener() {
+
+            // This method will trigger on item Click of navigation menu
+            @Override
+            public boolean onNavigationItemSelected(MenuItem menuItem) {
+
+                //Check to see which item was being clicked and perform appropriate action
+                switch (menuItem.getItemId()) {
+                    //Replacing the main content with ContentFragment Which is our Inbox View;
+                    case R.id.home:
+                        navItemIndex = 0;
+                        CURRENT_TAG = VOLUNTEER;
+                        break;
+                    case R.id.activity_main_menu_reports:
+                        navItemIndex = 1;
+                        CURRENT_TAG = REPORTS;
+                        break;
+                    case R.id.activity_main_menu_profile:
+                        navItemIndex = 2;
+                        CURRENT_TAG = PROFILE;
+                        break;
+                    default:
+                        navItemIndex = 0;
+                        CURRENT_TAG = VOLUNTEER;
+                        break;
+                }
+
+                //Checking if the item is in checked state or not, if not make it in checked state
+                if (menuItem.isChecked()) {
+                    menuItem.setChecked(false);
+                } else {
+                    menuItem.setChecked(true);
+                }
+                menuItem.setChecked(true);
+
+                loadHomeFragment();
+
+                return true;
+            }
+        });
 
 
-        DrawerLayout drawer = (DrawerLayout) findViewById(R.id.drawer_layout);
-        ActionBarDrawerToggle toggle = new ActionBarDrawerToggle(this, drawer, toolbar, R.string.navigation_drawer_open, R.string.navigation_drawer_close);
-        drawer.addDrawerListener(toggle);
-        toggle.syncState();
+        ActionBarDrawerToggle actionBarDrawerToggle = new ActionBarDrawerToggle(this, drawerLayout, toolbar, R.string.openDrawer, R.string.closeDrawer) {
 
-        NavigationView navigationView = (NavigationView) findViewById(R.id.nav_view);
-        navigationView.setNavigationItemSelectedListener(this);
+            @Override
+            public void onDrawerClosed(View drawerView) {
+                // Code here will be triggered once the drawer closes as we dont want anything to happen so we leave this blank
+                super.onDrawerClosed(drawerView);
+            }
 
+            @Override
+            public void onDrawerOpened(View drawerView) {
+                // Code here will be triggered once the drawer open as we dont want anything to happen so we leave this blank
+                super.onDrawerOpened(drawerView);
+            }
+        };
 
+        //Setting the actionbarToggle to drawer layout
+        //noinspection deprecation
+        drawerLayout.setDrawerListener(actionBarDrawerToggle);
 
+        //calling sync state is necessary or else your hamburger icon wont show up
+        actionBarDrawerToggle.syncState();
     }
 
     @Override
     public void onBackPressed() {
-        DrawerLayout drawer = (DrawerLayout) findViewById(R.id.drawer_layout);
-        if (drawer.isDrawerOpen(GravityCompat.START)) {
-            drawer.closeDrawer(GravityCompat.START);
-        } else {
-            super.onBackPressed();
+        if (drawerLayout.isDrawerOpen(GravityCompat.START)) {
+            drawerLayout.closeDrawers();
+            return;
         }
+
+        // This code loads home fragment when back key is pressed
+        // when user is in other fragment than home
+        if (shouldLoadHomeFragOnBackPress) {
+            // checking if user is on other navigation menu
+            // rather than home
+            if (navItemIndex != 0) {
+                navItemIndex = 0;
+                CURRENT_TAG = VOLUNTEER;
+                loadHomeFragment();
+                return;
+            }
+        }
+
+        super.onBackPressed();
+    }
+
+    // show or hide the fab
+    private void toggleFab() {
+        if (navItemIndex == 0)
+            floatingActionButton.show();
+        else
+            floatingActionButton.hide();
     }
 
     @Override
-    public boolean onCreateOptionsMenu(Menu menu) {
-        // Inflate the menu; this adds items to the action bar if it is present.
-        getMenuInflater().inflate(R.menu.main, menu);
-        TextView textViewUsername = findViewById(R.id.username);
-        TextView textViewEmail = findViewById(R.id.email);
-        if(usr != null){
-            String username = usr.getDisplayName();
-            textViewUsername.setText(username);
-            String email = usr.getEmail();
-            textViewEmail.setText(email);
-        }
-        return true;
-    }
+    public void onFragmentInteraction(Uri uri) {
 
-    @Override
-    public boolean onOptionsItemSelected(MenuItem item) {
-        // Handle action bar item clicks here. The action bar will
-        // automatically handle clicks on the Home/Up button, so long
-        // as you specify a parent activity in AndroidManifest.xml.
-        int id = item.getItemId();
-
-        //noinspection SimplifiableIfStatement
-        if (id == R.id.action_settings) {
-            return true;
-        }
-
-        return super.onOptionsItemSelected(item);
-    }
-
-    @SuppressWarnings("StatementWithEmptyBody")
-    @Override
-    public boolean onNavigationItemSelected(MenuItem item) {
-        // Handle navigation view item clicks here.
-        int id = item.getItemId();
-
-        if (id == R.id.activity_main_menu_statistics) {
-            // Handle the camera action
-        } else if (id == R.id.activity_main_menu_profile) {
-
-        }
-        return true;
     }
 }
